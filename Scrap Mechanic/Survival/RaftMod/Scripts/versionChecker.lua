2153e7ef0f8c5512fe19ba796371a4766eaf4f6f
Kerman <k@kermanispretty.cc>
2022-04-30 09:12:36 -0500

    Change back to using description.json
    Use a custom field in description.json
    
    also grammer fix

--- Scrap Mechanic/Survival/RaftMod/Scripts/versionChecker.lua
+++ Scrap Mechanic/Survival/RaftMod/Scripts/versionChecker.lua
@@ -4,16 +4,16 @@ g_checkForUpdates = true -- delcare to check for updates
 function checkRaftVersion()
 	g_checkForUpdates = false -- set it here incase it errors out.
     
-    local success, data = pcall(sm.json.open, '$CONTENT_3df6725e-462a-47e3-92ed-e5b66883588c/verison.json' )
+    local success, data = pcall(sm.json.open, '$CONTENT_3df6725e-462a-47e3-92ed-e5b66883588c/description.json' )
 
 	if not success then return end -- If the file doesn't exist, don't bother checking the version
 
-	local modVersion = data.version
-	local needsUpdate = modVersion ~= nil and VERSION < modVersion -- compare using (float) numbers
+	local fileModVersion = data.fileModVersion
+	local needsUpdate = fileModVersion ~= nil and VERSION < fileModVersion -- compare using (float) numbers
 	
 	if needsUpdate then
-		sm.gui.chatMessage("[Raft Mechanic] Your on version " .. tostring(VERSION) .. " and version " .. tostring(modVersion) .. " is available, please update!" )
+		sm.gui.chatMessage("[Raft Mechanic] You're on version " .. tostring(VERSION) .. " and version " .. tostring(fileModVersion) .. " is available, please update!" )
 	end
 
-    sm.log.info("[Raft Mechanic] You are on version: " .. tostring(VERSION) .. " and version.json is: " .. tostring(modVersion) )
+    sm.log.info("[Raft Mechanic] You're on version: " .. tostring(VERSION) .. " and version.json is: " .. tostring(fileModVersion) )
 end
